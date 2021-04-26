349b6a3d8f3150f070da8d56f8387f9424bfa49a
AR Shahin <mdshahinmije96@gmail.com>
2021-04-26 19:14:46 +0600

    twaillind setup

---
+++ src/components/MyNav.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="bg-purple-800 text-white py-3 mb-5"> 
+            <div class="container">
+                <router-link to="/">
+                <div class="text-3xl md:text-3xl font-bold mb-3">
+                 <i class="fa fa-viruses"></i> Covid-19 Tracker
+                </div>
+                </router-link>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        home
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        created(){
+            axios.get('https://api.covid19api.com/')
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
